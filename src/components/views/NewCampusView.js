22e0c350208ac2a3efd03c4f738c1127db9d4f7d
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2024-04-23 21:53:20 -0400

    updates with campus

--- src/components/views/NewCampusView.js
+++ src/components/views/NewCampusView.js
@@ -70,8 +70,8 @@ const NewCampusView = (props) => {
             <input type="text" name="imageUrl" onChange={(e) => handleChange(e)} />
             <br/>
             <br/>
-
-            <Button variant="contained" color="primary" type="submit">
+            // Button to submit the form
+            <Button variant="contained" color="primary" type="submit"> 
               Submit
             </Button>
             <br/>
